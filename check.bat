bbb6b303664fc8333f03322de0e8f5acab0bdb0d
Colinxu2020 <63941938+colinxu2020@users.noreply.github.com>
2025-08-20 23:11:35 +0800

    Update check.bat

--- check.bat
+++ check.bat
@@ -1,6 +1,6 @@
 @echo off
 adb shell "su -c "dd -if /dev/block/by-name/expdb -of /sdcard/expdb_dump.img""
 adb pull /sdcard/expdb_dump.img
-grep -a -A 3 "\[PART\] img_auth_required" expdb_dump.img
+findstr /L "[PART]" expdb.img | findstr /I /R "im[(ag)g]"
 echo -------
 echo Your Device is vulnerable by fenrir, if and only if you saw `img_auth_required = 0` on the above text
